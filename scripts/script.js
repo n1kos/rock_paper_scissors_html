0014359a42aa3772c3214154b1ec7aa41319d594
nik <nik@HAL>
2016-05-21 21:12:53 +0300

    building effect + more :)

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+;(function(){
+    function onReady(callback) {
+        var intervalID = window.setInterval(checkReady, 1000);
+
+        function checkReady() {
+            if (document.getElementsByTagName('body')[0] !== undefined) {
+                window.clearInterval(intervalID);
+                callback.call(this);
+            }
+        }
+    }
+
+    function show(id, value) {
+        document.getElementById(id).style.display = value ? 'block' : 'none';
+    }
+
+    onReady(function () {
+        show('ip-content', true);
+        show('ip-loading', false);
+        document.getElementsByClassName("ip-tv")[0].classList.add("ip-is-loaded");
+    });    
+})();
