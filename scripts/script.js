4d8208c0d3e42418b8c1c620187c75b6cc1e5b7f
nik <nik@HAL>
2016-05-22 02:04:22 +0300

    better structure, still listener wont remove

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,26 @@
 ;(function(){
+    /**
+     *
+     * helper functions to handle simple css properties with js
+     *
+     */    
+    function show(id, value) {
+        document.getElementById(id).style.display = value ? 'block' : 'none';
+    }
+
+    function showOpacity(id, value) {
+        document.getElementById(id).style.opacity = value ? '1' : '0';
+    }
+
+    function showVisibility(id, value) {
+        document.getElementById(id).style.visibility = value ? 'visible' : 'hidden';
+    }
+
+    /**
+     *
+     * cross browser event listener adding
+     *
+     */    
     var pfx = ["webkit", "moz", "MS", "o", ""];
     function PrefixedEvent(element, type, callback) {
         for (var p = 0; p < pfx.length; p++) {
@@ -7,31 +29,44 @@
         }
     };
 
-    // forEach method, could be shipped as part of an Object Literal/Module
+    /**
+      *
+      * forEach method, could be shipped as part of an Object Literal/Module
+      *
+      */      
     var forEach = function (array, callback, scope) {
       for (var i = 0; i < array.length; i++) {
         callback.call(scope, i, array[i]); // passes back stuff we need
       }
     };
 
+    /**
+     *
+     * make any node list to animate its elements one by one. removes listeners when done
+     *
+     */    
     function animateItemsSequence (parentNodeClass, animationType) {
         var myNodeList = document.getElementsByClassName(parentNodeClass)[0].children;
         
+        /* named function so it can be removed from the listener */       
+        var boundFunction = function (index, value) {
+            // value.classList.remove("animated");
+            try {
+                myNodeList[index+1].classList.add("animated", animationType);
+            }
+            catch (err) {
+                console.log('out of elements');
+            }
+        };
+
+        /*  loop thru each element and add cross-browser event listener for animation end */        
         forEach(myNodeList, function (index, value) {
             // console.log(index, value); // passes index + value back!
             if (index == 0) {
                 value.classList.add("animated", animationType);
             }
-            PrefixedEvent(value, "animationend", function(){
-                // value.classList.remove("animated", animationType);
-                try {
-                    myNodeList[index+1].classList.add("animated", animationType);
-                }
-                catch (err) {
-                    console.log('out of elements');
-                }
-                value.removeEventListener(animationType, arguments.callee);
-            });
+            PrefixedEvent(value, "animationend", boundFunction.bind(this, index, value), true);
+            value.removeEventListener("animationend", boundFunction, true);
         });
     };
 
@@ -44,18 +79,6 @@
                 callback.call(this);
             }
         }
-    }
-
-    function show(id, value) {
-        document.getElementById(id).style.display = value ? 'block' : 'none';
-    }
-
-    function showOpacity(id, value) {
-        document.getElementById(id).style.opacity = value ? '1' : '0';
-    }
-
-    function showVisibility(id, value) {
-        document.getElementById(id).style.visibility = value ? 'visible' : 'hidden';
     }
 
     onReady(function () {
